4f25de47ba31cd65ee7dca17d176a5106a375c9b
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-07-11 14:23:08 +0400

    Update py.py

--- py.py
+++ py.py
@@ -181,11 +181,4 @@ def radiospektr():
     return render_template_string(radiospektr_html)
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
