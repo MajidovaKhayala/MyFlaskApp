3ff10f45a466981eecbd668e044491201da3b76c
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-07-11 14:47:06 +0400

    Update py.py

--- py.py
+++ py.py
@@ -180,5 +180,5 @@ def poct():
 def radiospektr():
     return render_template_string(radiospektr_html)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
