fee3f4c6a7d7bc50937dfc18c8f2b38d8699fc78
MajidovaKhayala <43636499+MajidovaKhayala@users.noreply.github.com>
2025-07-13 08:26:44 +0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,17 @@
 function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
+  const fullLogo = document.getElementById('logo-full');
+  const miniLogo = document.getElementById('logo-mini');
+
   sidebar.classList.toggle('collapsed');
+
+  const collapsed = sidebar.classList.contains('collapsed');
+
+  if (collapsed) {
+    fullLogo.style.display = 'none';
+    miniLogo.style.display = 'block';
+  } else {
+    fullLogo.style.display = 'block';
+    miniLogo.style.display = 'none';
+  }
 }
